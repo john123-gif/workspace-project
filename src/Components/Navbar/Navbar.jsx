178cb9ee2a27c3f23e7b85bc91efd523489c5df8
John Teye <teyejohn167@gmail.com>
2024-01-20 22:56:56 +0000

    feat: implemented the navigation bar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import './Navbar.css'
+import navArrow from '../../assets/arrow-image.png';
+
+export const Navbar = () => {
+    return(
+        <nav className="navigation-bar">
+            <img src={navArrow} alt="" className="nav-arrow"/>
+           <span className="list-btn-span">
+            <ul className="nav-list">
+                    <a href="">
+                        <li>Home</li>
+                    </a>
+                    <a href="">
+                        <li>Properties</li>
+                    </a>
+                    <a href="">
+                        <li>How it works</li>
+                    </a>
+                </ul>
+                <button className="register-btn">
+                    Login/Register
+                </button>
+           </span>
+        </nav>
+    )
+}
