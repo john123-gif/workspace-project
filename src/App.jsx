c31865e9f071ca0186c833f2b734863daa27abae
John Teye <teyejohn167@gmail.com>
2024-02-02 21:01:25 +0000

    feat: implemented the steps page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navbar } from './Components/Navbar/Navbar'
 import { WelcomePage } from './Components/WelcomePage/WelcomePage'
 import { PortfolioPage } from './Components/PortfolioPage/PortfolioPage'
 import { AboutPage } from './Components/AboutPage/AboutPage'
+import { StepsPage } from './Components/StepsPage/StepsPage'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <WelcomePage />
       <PortfolioPage />
       <AboutPage />
+      <StepsPage />
     </div>
   )
 }
---
+++ src/Components/StepsPage/StepsPage.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import stepshouse from '../../assets/steps-page-image.png'
+import './StepsPage.css'
+
+export const StepsPage = () => {
+    return(
+        <div className="steps-page">
+            <div className="steps-div1">
+                <div className="header">
+                    <h2>HOW IT WORKS</h2>
+                    <p>
+                        We carefully select projects and co-invest
+                        with you<br/> because our success is based on your 
+                        success in the<br/> first place.
+                    </p>
+                </div>
+                <img src={stepshouse} alt="" />
+            </div>
+            <div className="steps-div2">
+                <span className="step step1">
+                    <p>/01</p>
+                    <h3>Register</h3>
+                    <p>
+                        Create a new account, get verified
+                        and gain access<br/> to the full platform’s
+                        capabilities.
+                    </p>
+                </span>
+
+                <span className="step step2">
+                    <p>/02</p>
+                    <h3>Invest or Fundraise</h3>
+                    <p>
+                    Invest in top-tier commercial real 
+                    estate deals or<br/> pitch your fundraising
+                     project
+                    </p>
+                </span>
+
+                <span className="step step3">
+                    <p>/03</p>
+                    <h3>Reward</h3>
+                    <p>
+                        Make stable investment returns or 
+                        fund the project<br/> with alternative 
+                        financing.
+                    </p>
+                </span>
+            </div>
+        </div>
+    )
+}
